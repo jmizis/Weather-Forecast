4657c5969e968501709cedcddd66304ceb930014
jmizis <107073756+jmizis@users.noreply.github.com>
2022-09-07 22:49:28 -0400

    Final Work

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Weather-Forecast
+
+## Description
+
+This challenge wanted us to create a weather forecast application that would show us the hourly forecast followed by a five day forecast of a any city you searched. There was no starter code given to us, we started from scratch. First I started with the html making sure I have my html linked with bootscrap and css. Next I created my title named "Weather Forecast" and next came the header for the site. Next came the main where you can find the search-input for the city you will be searching followed by two more sections. First will be the current weather of the city searched followed by the 5 day forecast. At the bottom of the html you can find my scripts for jquery, moment, and my script.js
+
+
+
+## API 
+
+For the api you were given the site to go on: https://openweathermap.org/api/one-call-api. On the open weather map I was able to find three diferent api calls to use. As you can see in my script.js I used one for the search value, one for the hourly forecast, and another one for my five day forecast. 
+
+
+## Github 
+
+On Github I was able to create a new repository where I was able to push all my work to. Before even starting the challenge I had to clone the new repository to the my computer first. I cloned it using my gitbash, from there on any code I worked on visual studio code I was able to save it and push it back to my repository. Once all my work was done I did a final push and created a deployed site. 
+
+
+
+
+## Conclusion
+
+This challenge was tougher than the previous ones but it's a great way to show off our newly learned shill to be able to work with api's. Finding a api is the easy part but picking it apart in the concole can be tough. But at the end I took my time and looked through all the information that was giving me and I was able to find the data I needed to complete the challenge. 
+
+## Links 
+repository: https://github.com/jmizis/Weather-Forecast
+Deployed site: https://jmizis.github.io/Weather-Forecast/
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var searchButton = document.querySelector("#searchBtn")
 searchButton.addEventListener("click", function(event){
 event.preventDefault()
    console.log ("search")
+   // This will clear out the current city forecast when you search a new one so it won't show multiple cities at once.
    $("#currentWeather").empty()
    $("#forecast").empty()
     var searchValue = document.getElementById ("search-input").value
@@ -12,9 +13,9 @@ event.preventDefault()
     historyBtn(searchValue)
 }) 
 
-
+// Creating local storage to save the cities
 var cityArray= JSON.parse(localStorage.getItem("cities"))||[]
-
+// This creates list of searched cities you viewed and will show on the page one at a time.
 function historyBtn(searchValue){
   var btn = $("<li>").addClass("list-group-item").text(searchValue)
   $("#searchHistory").append(btn)
@@ -22,7 +23,7 @@ function historyBtn(searchValue){
 for (var i =0; i<cityArray.length;i++){
   historyBtn(cityArray[i])
 }
-
+// This takes the city value from search input using latitude and longitude for location.
 function geoCode(searchValue){
   cityArray.push(searchValue)
   localStorage.setItem("cities",JSON.stringify(cityArray))
@@ -50,7 +51,7 @@ function geoCode(searchValue){
 
   
 
-
+// Will give you the hourly weather forecast for the city search.
 function citySearch(lat,lon){
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=366fd2554befabfba698a6df5073c21e&units=imperial` 
    
@@ -60,6 +61,7 @@ function citySearch(lat,lon){
     })
     .then(function (data) {
       console.log(data)
+      // Creates the weather card to display the current temp, wind, humidity which im pulling from the object in my console. 
       var weatherCard = $("<div>").addClass("card")
 var currentTemp = $("<h2>").text("Temp: "+data.main.temp + "°F")
 var wind = $("<h3>").text("Wind: "+data.wind.speed+ "MPH")
@@ -74,7 +76,7 @@ weatherCard.append(currentTemp,wind,humidity)
 }
 
 
-
+// Creates the weather forecast for the next five day and displays them. 
 function forecast(lat,lon){
   var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&exclude=5day&appid=366fd2554befabfba698a6df5073c21e&units=imperial`
   
@@ -85,6 +87,7 @@ function forecast(lat,lon){
   
  .then (function(data){
   console.log(data)
+  // Foreloop created for the five day forecast. API is calling weather forecast for every three hours so im starting on the fifth index midday and selecting eight index's from the fifth which equals 24 hours from the time selected. 
    for (var i = 5; i < data.list.length; i=i+8) {
 
   var fiveday = $("<div>").addClass("card")
